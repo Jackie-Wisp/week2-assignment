9816d2b3557f0e34c2c53f8085f8e749a18c43c7
Jackie-Wisp <jackmiddletonjpm1@gmail.com>
2024-12-15 21:46:19 +0000

    Pushing once before submission, IBS has stopped me from doing as much as I wanted and I will continue to work on this when I am able

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const images = [
     {src: "./Giyu.jpg", alt: "The character Giyu from The Demon Slayer anime/manga series"},
     {src: "./Muichiro.jpg", alt: "The character Muichiro from The Demon Slayer anime/manga series"},
 ]
+     
 
 document.getElementById("thumbnailContainer");
 document.getElementById("largeImageContainer");
 
 function createThumbnails(imagesarray) {
+    //I need to create more than one thumbnail --> I can use a loop
     imagesarray.forEach (function (imgitem) {
+          //You can use different ways to loop thorugh your array: for loop / forEach()
             const newImg = document.createElement("img");
             newImg.src = imgitem.src;
             newImg.alt = imgitem.alt;
@@ -22,15 +25,16 @@ function createThumbnails(imagesarray) {
         }
     )
     }
-      //I need to create more than one thumbnail --> I can use a loop
-  //You can use different ways to loop thorugh your array: for loop / forEach()
-  //Inside the loop, we have a few steps to do:
+//Inside the loop, we have a few steps to do:
   //create an image element
   //we need assign values to the image element properties
   //src = value
   //we also need to assign a value to the className property
   //we need add an event to the image elements we are creating here
   //we need to append the images to the thumbnail container
+ 
+
+
     
 
 
